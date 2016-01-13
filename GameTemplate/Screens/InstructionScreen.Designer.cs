dff39e7bca8bc16c2405a148abc2259c8cb5e348
Lukefischer <lukefisc@ed.amdsb.ca>
2016-01-13 14:00:20 -0500

    instruction screen design changes

--- GameTemplate/Screens/InstructionScreen.Designer.cs
+++ GameTemplate/Screens/InstructionScreen.Designer.cs
@@ -36,8 +36,8 @@
             // 
             this.exitButton.BackColor = System.Drawing.Color.YellowGreen;
             this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.exitButton.Location = new System.Drawing.Point(217, 393);
-            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Location = new System.Drawing.Point(172, 287);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(220, 72);
             this.exitButton.TabIndex = 11;
@@ -49,23 +49,23 @@
             // 
             this.instructionsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.instructionsLabel.ForeColor = System.Drawing.Color.DarkRed;
-            this.instructionsLabel.Location = new System.Drawing.Point(59, 7);
+            this.instructionsLabel.Location = new System.Drawing.Point(68, 0);
             this.instructionsLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.instructionsLabel.Name = "instructionsLabel";
-            this.instructionsLabel.Size = new System.Drawing.Size(493, 382);
+            this.instructionsLabel.Size = new System.Drawing.Size(658, 291);
             this.instructionsLabel.TabIndex = 12;
             // 
             // InstructionScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.Black;
             this.Controls.Add(this.instructionsLabel);
             this.Controls.Add(this.exitButton);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "InstructionScreen";
-            this.Size = new System.Drawing.Size(717, 483);
+            this.Size = new System.Drawing.Size(578, 375);
             this.ResumeLayout(false);
 
         }
